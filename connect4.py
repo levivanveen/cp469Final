889d217a1deeb220c86ea9accb8bdefb3f9b5707
mattcurrie77 <55199802+mattcurrie77@users.noreply.github.com>
2024-03-24 18:26:05 -0400

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -2,6 +2,7 @@ import numpy as np
 import pygame
 import sys
 import math
+import random
 
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
@@ -10,6 +11,15 @@ BLACK = (0, 0, 0)
 
 ROW_COUNT = 6
 COL_COUNT = 7
+
+PLAYER = 0
+AI = 1
+
+EMPTY = 0
+PLAYER_PIECE = 1
+AI_PIECE = 2
+
+WINDOW_LENGTH = 4
 
 def create_board():
     board = np.zeros((ROW_COUNT, COL_COUNT))
@@ -62,18 +72,79 @@ def draw_board(board):
 
     for i in range(COL_COUNT):
         for j in range(ROW_COUNT):
-            if board[j][i] == 1:
+            if board[j][i] == PLAYER_PIECE:
                 pygame.draw.circle(screen, RED, (int(i*PLAYAREA+PLAYAREA/2), height-int(j*PLAYAREA+PLAYAREA/2)), RADIUS)
-            elif board[j][i] == 2:
+            elif board[j][i] == AI_PIECE:
                 pygame.draw.circle(screen, YELLOW, (int(i*PLAYAREA+PLAYAREA/2), height-int(j*PLAYAREA+PLAYAREA/2)), RADIUS)
     pygame.display.update()
+
+def evaluate_window(window, piece):
+    score = 0
+    
+    if window.count(EMPTY) == 4:
+        return 0
+    
+    if piece == PLAYER_PIECE:
+        opponent_piece = PLAYER_PIECE
+    else:
+        opponent_piece = AI_PIECE
+        
+    if window.count(piece) == 4:
+        score += 500
+    elif window.count(piece) == 3 and window.count(EMPTY) == 1:
+        score += 10
+    elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+        score += 3
+    
+    if window.count(opponent_piece) == 3 and window.count(EMPTY) == 1:
+        score -= 8
+    elif window.count(opponent_piece) == 2 and window.count(EMPTY) == 2:
+        score -= 4
+        
+    return score
+
+def score_position(board, piece):
+	score = 0
+
+	## Score center column
+	center_array = [int(i) for i in list(board[:, COL_COUNT//2])]
+	score += center_array.count(piece) * 3
+
+	## Score Horizontal
+	for r in range(ROW_COUNT):
+		row_array = [int(i) for i in list(board[r,:])]
+		for c in range(COL_COUNT-3):
+			window = row_array[c:c+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
+
+	## Score Vertical
+	for c in range(COL_COUNT):
+		col_array = [int(i) for i in list(board[:,c])]
+		for r in range(ROW_COUNT-3):
+			window = col_array[r:r+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
+
+	## Score posiive sloped diagonal
+	for r in range(ROW_COUNT-3):
+		for c in range(COL_COUNT-3):
+			window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
+			score += evaluate_window(window, piece)
+
+    ## Score negative sloped diagonal
+	for r in range(ROW_COUNT-3):
+		for c in range(COL_COUNT-3):
+			window = [board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
+			score += evaluate_window(window, piece)
+
+	return score
+
 
 
 
 board = create_board()
 print_board(board)
 game_over = False
-turn = 0
+turn = random.randint(PLAYER, AI)
 pygame.init()
 
 PLAYAREA = 100
@@ -98,7 +169,7 @@ while not game_over:
         if event.type == pygame.MOUSEMOTION:
             pygame.draw.rect(screen, BLACK, (0, 0, width, PLAYAREA))
             posx = event.pos[0]
-            if turn == 0:
+            if turn == PLAYER:
                 pygame.draw.circle(screen, RED, (posx, int(PLAYAREA/2)), RADIUS)
             else:
                 pygame.draw.circle(screen, YELLOW, (posx, int(PLAYAREA/2)), RADIUS)
@@ -106,31 +177,33 @@ while not game_over:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             pygame.draw.rect(screen, BLACK, (0, 0, width, PLAYAREA))
-            if turn == 0:
+            if turn == PLAYER:
                 posx = event.pos[0]
                 col = int(math.floor(posx/PLAYAREA))
 
                 if is_valid(board, col):
                     row = next_row(board, col)
-                    add_piece(board, row, col, 1)
+                    add_piece(board, row, col, PLAYER_PIECE)
 
-                    if win_check(board, 1):
+                    if win_check(board, PLAYER_PIECE):
                         label = font.render("Red wins!!", 1, RED)
                         screen.blit(label, (130, 10))
                         game_over = True
 
+            ## AI turn
             else:
                 posx = event.pos[0]
                 col = int(math.floor(posx/PLAYAREA))
 
                 if is_valid(board, col):
                     row = next_row(board, col)
-                    add_piece(board, row, col, 2)
+                    add_piece(board, row, col, AI_PIECE)
 
-                    if win_check(board, 2):
+                    if win_check(board, AI_PIECE):
                         label = font.render("Yellow wins!!", 1, YELLOW)
-                        screen.blit(label, (130, 10))
+                        screen.blit(label, (70, 10))
                         game_over = True
+                        
             print_board(board)
             draw_board(board)
 
