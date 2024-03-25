ccfbef2140cc2d94e7f9e96e87bb3ccf883f25b4
ClarkBen1225 <55252974+ClarkBen1225@users.noreply.github.com>
2024-03-25 12:46:24 -0400

    Add files via upload

--- connect4.py
+++ connect4.py
@@ -4,33 +4,36 @@ import sys
 import math
 import random
 
+#Colours for board and pieces
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
 
+#Board Sizing
 ROW_COUNT = 6
 COL_COUNT = 7
 
+#Represent whos turn it is
 PLAYER = 0
 AI = 1
 
 EMPTY = 0
 PLAYER_PIECE = 1
 AI_PIECE = 2
-
+#Window to check fo spaces and check for win or next move
 WINDOW_LENGTH = 4
 
 def create_board():
     board = np.zeros((ROW_COUNT, COL_COUNT))
     return board
-
+#Drops piece into given board either 1 for player or 2 for AI
 def add_piece(board, row, col, piece):
     board[row][col] = piece
-
+#Checks if the decided spot is a valid location
 def is_valid(board, col):
     return board[ROW_COUNT-1][col] == 0
-
+#Assures we are looking at the next open space in a colum
 def next_row(board, col):
     for i in range(ROW_COUNT):
         if board[i][col] == 0:
@@ -39,6 +42,7 @@ def next_row(board, col):
 def print_board(board):
     print(np.flip(board, 0))
 
+#Checks for all possible combos for a connect 4
 def win_check(board, piece):
     #Horizontal wim
     for i in range(COL_COUNT-3):
@@ -72,12 +76,13 @@ def draw_board(board):
 
     for i in range(COL_COUNT):
         for j in range(ROW_COUNT):
-            if board[j][i] == PLAYER_PIECE:
+            if board[j][i] == 1:
                 pygame.draw.circle(screen, RED, (int(i*PLAYAREA+PLAYAREA/2), height-int(j*PLAYAREA+PLAYAREA/2)), RADIUS)
-            elif board[j][i] == AI_PIECE:
+            elif board[j][i] == 2:
                 pygame.draw.circle(screen, YELLOW, (int(i*PLAYAREA+PLAYAREA/2), height-int(j*PLAYAREA+PLAYAREA/2)), RADIUS)
     pygame.display.update()
 
+#Looks at the current window and evaluates what the best options are
 def evaluate_window(window, piece):
     score = 0
     
@@ -138,7 +143,62 @@ def score_position(board, piece):
 
 	return score
 
+#When a game is won
+def terminal_node(board):
+     return win_check(board, PLAYER_PIECE) or win_check(board, AI_PIECE) or len(get_valid_locations(board)) == 0
 
+#Recursivly checks for best possible moves in a tree of a given depth and ignores loss cases using alpha beta pruning
+def minimax(board, depth, alpha, beta, maximizingPlayer):
+	valid_locations = get_valid_locations(board)
+	is_terminal = terminal_node(board)
+	if depth == 0 or is_terminal:
+		if is_terminal:
+			if win_check(board, AI_PIECE):
+				return (None, 100000000000000)
+			elif win_check(board, PLAYER_PIECE):
+				return (None, -10000000000000)
+			else: # Game is over, no more valid moves
+				return (None, 0)
+		else: # Depth is zero
+			return (None, score_position(board, AI_PIECE))
+	if maximizingPlayer:
+		value = -math.inf
+		column = random.choice(valid_locations)
+		for col in valid_locations:
+			row = next_row(board, col)
+			b_copy = board.copy()
+			add_piece(b_copy, row, col, AI_PIECE)
+			new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
+			if new_score > value:
+				value = new_score
+				column = col
+			alpha = max(alpha, value)
+			if alpha >= beta:
+				break
+		return column, value
+
+	else: # Minimizing player
+		value = math.inf
+		column = random.choice(valid_locations)
+		for col in valid_locations:
+			row = next_row(board, col)
+			b_copy = board.copy()
+			add_piece(b_copy, row, col, PLAYER_PIECE)
+			new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
+			if new_score < value:
+				value = new_score
+				column = col
+			beta = min(beta, value)
+			if alpha >= beta:
+				break
+		return column, value
+        
+def get_valid_locations(board):
+    valid_loc = []
+    for col in range(COL_COUNT):
+        if is_valid(board, col):
+            valid_loc.append(col)
+    return valid_loc
 
 
 board = create_board()
@@ -147,7 +207,7 @@ game_over = False
 turn = random.randint(PLAYER, AI)
 pygame.init()
 
-PLAYAREA = 100
+PLAYAREA = 100 #Size of a given slot in the board
 
 width = COL_COUNT * PLAYAREA
 height = (ROW_COUNT+1) * PLAYAREA
@@ -171,11 +231,9 @@ while not game_over:
             posx = event.pos[0]
             if turn == PLAYER:
                 pygame.draw.circle(screen, RED, (posx, int(PLAYAREA/2)), RADIUS)
-            else:
-                pygame.draw.circle(screen, YELLOW, (posx, int(PLAYAREA/2)), RADIUS)
         pygame.display.update()
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN: #Player Turn
             pygame.draw.rect(screen, BLACK, (0, 0, width, PLAYAREA))
             if turn == PLAYER:
                 posx = event.pos[0]
@@ -189,26 +247,29 @@ while not game_over:
                         label = font.render("Red wins!!", 1, RED)
                         screen.blit(label, (130, 10))
                         game_over = True
+                    
+                    turn += 1
+                    turn = turn % 2
 
-            ## AI turn
-            else:
-                posx = event.pos[0]
-                col = int(math.floor(posx/PLAYAREA))
+                    print_board(board)
+                    draw_board(board)
+            #AI Turn
+    if turn == AI and not game_over:
+        col, minimax_score = minimax(board, 2, -math.inf, math.inf, True)
 
-                if is_valid(board, col):
-                    row = next_row(board, col)
-                    add_piece(board, row, col, AI_PIECE)
+        if is_valid(board, col):
+            row = next_row(board, col)
+            add_piece(board, row, col, AI_PIECE)
 
-                    if win_check(board, AI_PIECE):
-                        label = font.render("Yellow wins!!", 1, YELLOW)
-                        screen.blit(label, (70, 10))
-                        game_over = True
-                        
+            if win_check(board, AI_PIECE):
+                label = font.render("Yellow wins!!", 1, YELLOW)
+                screen.blit(label, (130, 10))
+                game_over = True
             print_board(board)
             draw_board(board)
-
+            #Changes the turn from AI to player or vise versa
             turn += 1
             turn = turn % 2
 
-            if game_over:
-                pygame.time.wait(2500)
+    if game_over:
+        pygame.time.wait(2500)
